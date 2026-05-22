b9f8613efbe01655575d34108a23927bcb3707e0
Víctor Alonso <labra13@gmail.com>
2026-05-22 12:48:30 +0200

    android: native AndroidBridge.openUrl() via JSInterface para WhatsApp

--- android-agenda/android/app/src/main/java/com/tpv/agenda/MainActivity.java
+++ android-agenda/android/app/src/main/java/com/tpv/agenda/MainActivity.java
@@ -1,5 +1,26 @@
 package com.tpv.agenda;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.addJavascriptInterface(this, "AndroidBridge");
+        }
+    }
+
+    @JavascriptInterface
+    public void openUrl(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
